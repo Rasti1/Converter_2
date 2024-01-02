7f20421612e91518684ee28571d491329586ab9c
Rasti1 <rostrostov1999@gmail.com>
2024-01-02 16:10:01 +0200

    Add variables and methods in develop

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,9 +3,24 @@ package app;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Converter mile-kilometer/kilometer-mile");
+    static double num;
+    static double result;
+    static Scanner sc = new Scanner(System.in);
+    static final double kilometerToMile = 0.62;
 
+    public static void main(String[] args) {
+        System.out.println("Converter mile-kilometer/kilometer-mile\n");
+        String info = getInfo();
+        System.out.print(info);
     }
+
+    private static String getInfo(){
+        return "Enter number of kilometres: ";
+    }
+
+    private static double convertKilometresToMiles (double num){
+        return num * kilometerToMile;
+    }
+
 
 }
