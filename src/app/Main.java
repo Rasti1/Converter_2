2ae6e47c07c7064df3d6fc9d3514e434588679bd
Rasti1 <rostrostov1999@gmail.com>
2024-01-02 16:04:30 +0200

    Add try/catch check

--- src/app/Main.java
+++ src/app/Main.java
@@ -10,11 +10,22 @@ public class Main {
     static final double mileToKilometer = 1.61;
 
     public static void main(String[] args) {
-        System.out.println("Converter mile-kilometer/kilometer-mile");
+        System.out.println("Converter mile-kilometer/kilometer-mile\n");
         String info = getInfo();
         System.out.print(info);
-
+        try{
+            num = sc.nextDouble();
+            if (num < 0){
+                System.out.println("Impossible value of miles. ");
+            } else {
+                result = convertMilesToKilometres(num);
+                System.out.printf("Kilometers equals is %.2f ", result);
+            }
+        } catch(Exception e) {
+            System.out.println("Invalid input. Please enter a valid number.");
+        }
     }
+
     private static String getInfo(){
         return "Enter number of miles: ";
     }
