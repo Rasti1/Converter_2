b3bdbe0738dce5a3477b1fac944403d9fa0213a0
Rasti1 <rostrostov1999@gmail.com>
2024-01-02 15:51:17 +0200

    Add variables and methods

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,25 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static double num;
+    static double result;
+    static Scanner sc = new Scanner(System.in);
+    static final double mileToKilometer = 1.61;
+
     public static void main(String[] args) {
         System.out.println("Converter mile-kilometer/kilometer-mile");
 
     }
+    private static String getInfo(){
+        return "Enter number of miles: ";
+    }
+
+    private static double convertMilesToKilometres (double num){
+        return num * mileToKilometer;
+    }
+
 
 }
